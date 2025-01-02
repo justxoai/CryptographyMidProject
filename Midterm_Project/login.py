7173e32e3b309d4362c03271b27be91858f78d8d
Nguyễn Thái Bình <120584598+Thai-Binh727@users.noreply.github.com>
2025-01-02 10:39:14 +0700

    update login

--- Midterm_Project/login.py
+++ Midterm_Project/login.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from Midterm_Project.otp import check_otp
 
 login = Flask(__name__)
 
 @login.route("/")
 def login_function():
     return render_template("login.html")
+
+@login.route('/verify', methods = ['POST'])
+def verify_otp():
+    otp = request.form.get('password')
+    if check_otp(otp):
+        return render_template('index.html')
+    else:
+        return 'no'
 
 if __name__ == "__main__":
     login.run(debug=True)
