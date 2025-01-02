a279e46ff302256a68643f453e5d315bf24dc852
Nguyễn Thái Bình <120584598+Thai-Binh727@users.noreply.github.com>
2025-01-02 10:38:04 +0700

    totp generator

---
+++ Midterm_Project/otp.py
@@ -0,0 +1,14 @@
+import pyotp
+import qrcode
+
+# secret_key = pyotp.random_base32()
+# print(secret_key)
+secret_key = '24ZLU6I7ZNXIJP6ZFDGFOKTFIYVCN5K3'
+totp = pyotp.TOTP(secret_key)
+gg_uri = totp.provisioning_uri(name = 'test@gmail.com', issuer_name = 'test gg auth')
+qr = qrcode.make(gg_uri)
+qr.show()
+
+def check_otp(otp):
+    if totp.verify(otp, valid_window=1):
+        return True
